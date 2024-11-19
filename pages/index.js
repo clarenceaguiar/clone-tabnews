a765449b84091f8d6e3dfd24033cb011e0a7cab4
Clarence Silva Aguiar <112132378+clarenceaguiar@users.noreply.github.com>
2024-11-19 21:06:32 +0000

    altera a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Mirian, amo muito você! Se você me ama, dê um sorriso!</h1>
+    return <h1>Mirian, amo muito você! Se você me ama, dê um sorriso!&#x1F48C;</h1>
 }
 
 export default Home;
